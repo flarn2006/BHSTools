11330e1b6d09da56c3093c8afef54ab7990d2378
flarn2006 <flarn2006@gmail.com>
2018-12-15 04:32:44 -0500

    fix: add missing HTML close tag

--- devtest.py
+++ devtest.py
@@ -5,7 +5,7 @@ class TestDevice(VirtDevice):
 		print('{:04X} ( {} )'.format(cmd, arg))
 
 bus = Interface('/dev/ttyUSB0')
-dev = TestDevice(2, 3111, fromhex('00 00 00 AB CD EF'), 0xFFFF, 0xFFFF)
+dev = TestDevice(0x06, 3249, fromhex('00 00 00 AB CD EF'), 0xFFFF, 0xFFFF)
 dev.xmitters.append(bus.write)
 
 while True:
