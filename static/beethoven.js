a880159d161745f7a5925c4db2f10990c7c8f3f0
flarn2006 <flarn2006@gmail.com>
2019-01-27 17:23:47 -0500

    docs: explain seemingly-random filename

--- static/beethoven.js
+++ static/beethoven.js
@@ -1,3 +1,6 @@
+// In case you're curious about the filename, it's a reference to the official downloading software, known as Vivaldi.
+// (If anyone knows where I can find a copy of that software, please let me know!)
+
 window.onload = function() {
 	document.getElementById('dlstart').addEventListener('click', function() {
 		var xhr = new XMLHttpRequest();
