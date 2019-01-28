6da302a987e7b049396dcac81578b93d757004b3
flarn2006 <flarn2006@gmail.com>
2019-01-28 05:29:55 -0500

    feat: add descriptive titles for config blocks

--- config_rpt_util.py
+++ config_rpt_util.py
@@ -1,4 +1,5 @@
 from intellibus import tohex
+import struct
 
 def fmt_16bit_hex(n):
 	return '{:04X}'.format(n)
@@ -19,6 +20,34 @@ def make_hexdump(data, width=32):
 		rows.append((addr_str, data_row_str, ascii_str))
 	
 	return rows
+
+def describe_config_block(cmd, arg):
+	def int16(offset):
+		return struct.unpack('<H', arg[offset:offset+2])[0]
+	
+	name = 'Unknown Data'
+	if cmd == 0xC8:
+		name = 'Panel Configuration'
+	elif cmd == 0xC9:
+		name = 'Communicator {}'.format(int16(2) + 1)
+	elif cmd == 0xCA:
+		name = 'Account {}'.format(arg[1] + 1)
+	elif cmd == 0xCC:
+		name = 'Alarm Output'
+	elif cmd == 0xD0:
+		name = 'User {}'.format(int16(2) + 1)
+	elif cmd == 0xD1:
+		name = 'Zone {}'.format(arg[3] + 1)
+	elif cmd == 0xD3:
+		name = 'Device {0} (0x{0:04X})'.format(int16(18))
+	elif cmd == 0xD5:
+		name = 'Input'
+	elif cmd == 0xD7:
+		name = 'Installer User'
+	elif cmd == 0xDA:
+		name = 'Script {}'.format(int16(4) + 1)
+	
+	return '[{:04X}] {}'.format(cmd, name)
 
 if __name__ == '__main__':
 	print('This Python script is designed to be imported as a library, not run directly.')
