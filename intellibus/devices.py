2e1faa966f978fdc4159dcdf3c03b7ced9f0189c
flarn2006 <flarn2006@gmail.com>
2019-04-22 20:32:22 -0400

    feat: start of virtual keypad

---
+++ intellibus/devices.py
@@ -0,0 +1,68 @@
+from queue import Queue
+from intellibus import *
+
+class Keypad(VirtDevice):
+	def __init__(self, ibus:Intellibus, kind:int, model:int, serial_no:bytes, hdw_conf:int=0, fw_ver:(int,int)=0, addr:int=None):
+		super().__init__(ibus, kind, model, serial_no, hdw_conf, fw_ver, addr)
+		self.keyqueue = Queue()
+	
+	def on_ping(self):
+		while not self.keyqueue.empty():
+			keycode = self.keyqueue.get()[:2].rjust(2, '0')
+			if keycode.upper() in '3C BC 68 E8 3E BE 1B 9B 73 F3 08 88 2D AD 2B AB'.split(' '):
+				keycode = '1B ' + keycode
+			self.send(0x7E4, fromhex(keycode))
+	
+	def key(self, keycode):
+		self.keyqueue.put(keycode)
+
+
+class IconKeypad(Keypad):
+	def __init__(self, ibus:Intellibus, serial_no:bytes, hdw_conf:int=0, fw_ver:(int,int)=0, model:int=3101):
+		super().__init__(ibus, 1, model, serial_no, hdw_conf, fw_ver)
+		self.acct = 0
+		self.lcd = [False] * 64
+	
+	def handle_cmd(self, cmd, arg):
+		if cmd == 0x7DA and len(arg) >= 9 and arg[0] == self.acct:
+			for index in range(64):
+				byte = index // 8
+				bit = index % 8
+				self.lcd[index] = (arg[byte+1] & (1<<bit) > 0)
+
+
+class Programmer(Keypad):
+	def __init__(self, ibus):
+		super().__init__(ibus, 5, 3121, fromhex('00 00 FF FF FF FF'), 0, (7,1), 0x7FFE)
+		self.display = b' ' * 16 * 4
+		self.ping_counter = 0
+		self.curX = 0
+		self.curY = 0
+		self.curVis = False
+
+	def writestr(self, index, src):
+		dest = self.display
+		self.display = dest[:index] + src[:len(dest)-index] + dest[index+len(src):]
+
+	def handle_cmd(self, cmd, arg):
+		if cmd == 0x7DC:
+			text = arg[3:].rstrip(b'\0')
+			if b'\x0c' in text:
+				self.display = b' '*16*4
+				self.curX = 0
+				self.curY = 0
+			else:
+				acct, row, col = struct.unpack('<bbb', arg[:3])
+				if col & 0x80:
+					col &= 0x7F
+					self.curVis = True
+				else:
+					self.curVis = False
+				if col > 0:
+					self.curY = col - 1
+				if row > 0:
+					self.curX = row - 1
+				self.writestr(16*self.curY + self.curX, text)
+				self.curX += len(text)
+				self.curY += self.curX // 16
+				self.curX %= 16
--- s3121.py
+++ s3121.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 from sys import argv, exit
 from threading import Thread
-from queue import Queue
 from intellibus import *
+import intellibus.devices as devices
 import flask
 import json
 import config_rpt_util
@@ -19,15 +19,22 @@ def flask_index():
 
 @app.route('/status')
 def flask_status():
-	global pgm, dl
+	global pgm, dl, kp
 	cpos = 16 * pgm.curY + pgm.curX
-	d = {'text':process_display_str(pgm.display), 'cursor':{'pos':cpos, 'visible':pgm.curVis}, 'dl_complete':dl.complete}
+	lcdbits = ''.join(['1' if b else '0' for b in kp.lcd])
+	d = {'text':process_display_str(pgm.display), 'cursor':{'pos':cpos, 'visible':pgm.curVis}, 'dl_complete':dl.complete, 'keypad_lcd':lcdbits}
 	return (json.dumps(d), {'Content-Type': 'application/json'})
 
 @app.route('/key', methods=['POST'])
 def flask_key():
 	global pgm
 	pgm.key(flask.request.data.decode('ascii'))
+	return 'Done'
+
+@app.route('/kpkey', methods=['POST'])
+def flask_kpkey():
+	global kp
+	kp.key(flask.request.data.decode('ascii'))
 	return 'Done'
 
 @app.route('/start_download', methods=['POST'])
@@ -47,9 +54,6 @@ def flask_config_rpt():
 	global dl
 	return flask.render_template('config_rpt.html', results=dl.results, util=config_rpt_util)
 
-def writestr(dest, index, src):
-	return dest[:index] + src[:len(dest)-index] + dest[index+len(src):]
-
 def process_display_str(text):
 	try:
 		return text.decode('ascii')
@@ -57,49 +61,6 @@ def process_display_str(text):
 		out = ''
 		for b in text:
 			out += chr(b) if 32<=b<=126 else '.'
-
-class Programmer(VirtDevice):
-	def __init__(self, ibus):
-		super().__init__(ibus, 5, 3121, fromhex('00 00 FF FF FF FF'), 0, (7,1), 0x7FFE)
-		self.display = b' ' * 16 * 4
-		self.ping_counter = 0
-		self.curX = 0
-		self.curY = 0
-		self.curVis = False
-		self.keyqueue = Queue()
-
-	def on_ping(self):
-		while not self.keyqueue.empty():
-			keycode = self.keyqueue.get()[:2].rjust(2, '0')
-			if keycode.upper() in '3C BC 68 E8 3E BE 1B 9B 73 F3 08 88 2D AD 2B AB'.split(' '):
-				keycode = '1B ' + keycode
-			self.send(0x7E4, fromhex(keycode))
-
-	def handle_cmd(self, cmd, arg):
-		if cmd == 0x7DC:
-			text = arg[3:].rstrip(b'\0')
-			if b'\x0c' in text:
-				self.display = b' '*16*4
-				self.curX = 0
-				self.curY = 0
-			else:
-				acct, row, col = struct.unpack('<bbb', arg[:3])
-				if col & 0x80:
-					col &= 0x7F
-					self.curVis = True
-				else:
-					self.curVis = False
-				if col > 0:
-					self.curY = col - 1
-				if row > 0:
-					self.curX = row - 1
-				self.display = writestr(self.display, 16*self.curY + self.curX, text)
-				self.curX += len(text)
-				self.curY += self.curX // 16
-				self.curX %= 16
-	
-	def key(self, keycode):
-		self.keyqueue.put(keycode)
 
 class Downloader(VirtDevice):
 	def __init__(self, ibus):
@@ -146,8 +107,9 @@ webthread = Thread(target=app.run, kwargs={'host':'0.0.0.0', 'port':3121})
 
 try:
 	bus = Intellibus(argv[1], debug='rx,tx')
-	pgm = Programmer(bus)
+	pgm = devices.Programmer(bus)
 	dl = Downloader(bus)
+	kp = devices.IconKeypad(bus, fromhex('13 37 1D EC 0D ED'), 0x1337, (0,0))
 	webthread.start()
 	bus.run()
 except KeyboardInterrupt:
