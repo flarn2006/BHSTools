flarn2006/BHSTools

commit 6beb6406845acc36762c866df01c0a2556753a88

feat: misc modem testing

intellibus/main.py
@@ -107,7 +107,7 @@ class BasicInterface:
 			#	print('crc fail: ' + tohex(data + check))
 	
 	def write(self, packet):
-		self.send_bytes(b'\x1e' + bytes(packet).replace(b'\x7d', b'\x7d\x5d').replace(b'\x1e', b'\x7d\x3e'))
+		self.send_bytes(b'\x1e' + bytes(packet).replace(b'\x7d', b'\x7d\x5d').replace(b'\x1e', b'\x7d\x3e') + b'\x1e')
 
 class Interface(BasicInterface):
 	def __init__(self, port, baudrate=38400):

intellibus/modem.py
@@ -168,9 +168,6 @@ class VivaldiSession(Connection):
 		#TODO: add actual check
 		return True
 	
-	def offset_next_sync(self, a, b):
-		self.next_sync = ((self.next_sync[0] + a) & 7, (self.next_sync[1] + b) & 7)
-	
 	def read(self):
 		pkt, _ = super().read()
 		if type(pkt) is VivaldiMessage:
@@ -181,26 +178,17 @@ class VivaldiSession(Connection):
 			else:
 				if pkt.getcmd() == 4:
 					self.des_key = pkt.getarg().rstrip(b'\0').decode('ascii')
-			if self.is_server:
-				self.offset_next_sync(1, 0)
-			else:
-				self.offset_next_sync(0, 1)
 			sleep(.5)
-			self.send_sync()
 			self.waiting_for_sync = True
 		else:
 			if not self.is_server:
 				self.next_sync = (self.next_sync[0], pkt.sync[0])
-
-			if self.last_message_by_me:
-				sleep(.5)
-				#self.send_sync(min(pkt.sync[1] + 1, 2))
-				self.send_raw(VivaldiPacket(panel_id=self.panel_id, sync=(self.next_sync[1], (pkt.sync[1]+1)%3)))
-				sleep(.5)
-				self.send_next()
 			self.waiting_for_sync = False
 
 		return pkt, True
+	
+	def sync(self, a, b):
+		self.send_raw(VivaldiPacket(panel_id=self.panel_id, sync=(a, b)))
 	
 	def send_sync(self, status=2):
 		s = self.next_sync[0 if self.is_server else 1]

commit 65d6b7244518596436e36de293651958531ada05

fix: unresponsive Soft3121 in some cases
Regression introduced in 6beb6406845acc36762c866df01c0a2556753a88

intellibus/main.py
@@ -107,7 +107,7 @@ class BasicInterface:
 			#	print('crc fail: ' + tohex(data + check))
 	
 	def write(self, packet):
-		self.send_bytes(b'\x1e' + bytes(packet).replace(b'\x7d', b'\x7d\x5d').replace(b'\x1e', b'\x7d\x3e') + b'\x1e')
+		self.send_bytes(b'\x1e' + bytes(packet).replace(b'\x7d', b'\x7d\x5d').replace(b'\x1e', b'\x7d\x3e'))
 
 class Interface(BasicInterface):
 	def __init__(self, port, baudrate=38400):

intellibus/modem.py
@@ -64,6 +64,10 @@ class ModemInterface(BasicInterface):
 		sleep(1.5)
 		self.serial.write(b'ATH0\r\n')
 		self.hangup_in_progress = False
+	
+	def write(self, packet):
+		super().write(packet)
+		self.send_bytes(b'\x1e')
 
 class VivaldiPacket(Packet):
 	def __init__(self, body=b'', panel_id=0xFFFFFFFF, sync=(0, 0)):